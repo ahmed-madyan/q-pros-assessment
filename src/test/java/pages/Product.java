e4885d13d29e78865fea4b6afb2d01ccd9a06e7c
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-01-01 00:43:20 +0200

    Update Product.java

--- src/test/java/pages/Product.java
+++ src/test/java/pages/Product.java
@@ -1,4 +1,22 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import utilities.actions.AlertActions;
+import utilities.actions.ElementActions;
+
 public class Product {
-        }
+    private static final By addToCart_Button = By.xpath("//a[contains(text(), \"Add to cart\")]");
+
+    public static void addToCart(){
+        ElementActions.click(addToCart_Button);
+    }
+
+    public static void validateProductAddedSuccessfulMessageInConfirmationAlert(String expectedText) {
+        Assert.assertEquals(AlertActions.getAlertText(), expectedText);
+    }
+
+    public static void acceptSignUpConfirmAlert() {
+        AlertActions.acceptAlert();
+    }
+}
